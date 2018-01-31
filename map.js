4e9920709e1d3fd43f15e4bb4bbdd6a43a9e284d
Egor <egor.izukaev@gmail.com>
2018-01-31 11:19:45 +0300

    add vizulation lib

--- map.js
+++ map.js
@@ -1,33 +1,28 @@
 var map;
-var locations = [];
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 68.399, lng: 81.035},
-        zoom: 3
+        zoom: 2,
+        center: {lat: -33.865427, lng: 151.196123},
+        mapTypeId: 'terrain'
     });
 
     var script = document.createElement('script');
 
-    script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
+    script.src = 'GeoJSONP.js';
     document.getElementsByTagName('head')[0].appendChild(script);
-
-   window.eqfeed_callback = function(results) {
-        for (var i = 0; i < results.features.length; i++) {
-            var coords = results.features[i].geometry.coordinates;
-            var latLng = new google.maps.LatLng(coords[1],coords[0]);
-            var marker = new google.maps.Marker({
-                position: latLng,
-                map: map
-
-            });
-
-
-        }
-
-    }
-
-
 
 }
 
-
+function eqfeed_callback(results) {
+    var heatmapData = [];
+    for (var i = 0; i < results.features.length; i++) {
+        var coords = results.features[i].geometry.coordinates;
+        var latLng = new google.maps.LatLng(coords[1], coords[0]);
+        heatmapData.push(latLng);
+    }
+    var heatmap = new google.maps.visualization.HeatmapLayer({
+        data: heatmapData,
+        dissipating: false,
+        map: map
+    });
+}
