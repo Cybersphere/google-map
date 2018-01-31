40810cc6bb5794cf3b032d73618b395fdca88c71
klimenko <klimenko@ttcsoft.ru>
2018-01-31 13:58:30 +0300

    Some fix

--- map.js
+++ map.js
@@ -10,22 +10,37 @@ function initMap() {
 
     script.src = 'data.js';
     document.getElementsByTagName('head')[0].appendChild(script);
-
+	//eqfeed_callback();
 }
 
-function eqfeed_callback(results) {
-    var heatmapData = [];
+function eqfeed_callback(data) {
+	
+		
+	var heatmapData = [];
+	for(var id in data.pin){
+		var dotted = data.pin[id];
+		
+		var mapData = {
+			location: new google.maps.LatLng(dotted.latitude, dotted.longitude),
+			weight: parseFloat(dotted.doserate)
+		};
+		heatmapData.push(mapData);
+	}
+	
+	console.log(heatmapData)
+	var heatmap = new google.maps.visualization.HeatmapLayer({
+        data: heatmapData,
+        dissipating: false,
+        map: map
+    });
+    /* 
     for (var i = 0; i < results.pin.length; i++) {
         var lat = results.pin[i].latitude;
         var lng = results.pin[i].longitude;
         var latLng = new google.maps.LatLng(lat, lng);
 
         heatmapData.push(latLng);
-    }
-    var heatmap = new google.maps.visualization.HeatmapLayer({
-        data: heatmapData,
-        dissipating: false,
-        map: map
-    });
-    return console.log(heatmapData);
+    }+
+	console.log(heatmapData)
+     */
 }
